94dff0b41e62023d2153ca9afb208b37d9517a66
benscruton <benjamin.scruton@gmail.com>
2021-08-06 22:02:19 -0500

    Added descriptions for Dinner Getter project

--- src/img/projectDemos/dinnerGetter/index.ts
+++ src/img/projectDemos/dinnerGetter/index.ts
@@ -6,68 +6,75 @@ import dragAndDropAddr from "./dragAndDrop.ogg";
 import homePageAddr from "./homePage.png";
 import notLoggedInAddr from "./notLoggedIn.png";
 import recipePageAddr from "./recipePage.png";
+import shoppingListAddr from "./shoppingList.png";
 import storeModeAddr from "./storeMode.ogg";
 import Media from "../../../interfaces/Media";
 
 const category: Media = {
   address: categoryAddr,
-  altText: "",
+  altText: "Adding, editing, and removing categories from the categorized list",
   videoType: "video/ogg"
 };
 
 const recipe: Media = {
   address: recipeAddr,
-  altText: "",
+  altText: "Adding a new recipe to the database",
   videoType: "video/ogg"
 };
 
 const ingredient: Media = {
   address: ingredientAddr,
-  altText: "",
+  altText: "Adding and removing ingredients to the shopping list",
   videoType: "video/ogg"
 };
 
 const authZero: Media = {
   address: authZeroAddr,
-  altText: ""
+  altText: "Third-party login provided by Auth0"
 };
 
 const dragAndDrop: Media = {
   address: dragAndDropAddr,
-  altText: "",
+  altText: "Reorder ingredients, including across categories, with drag-and-drop functionality",
   videoType: "video/ogg"
 };
 
 const homePage: Media = {
   address: homePageAddr,
-  altText: ""
+  altText: "The home page of Dinner Getter, featuring the user's pantry and suggested recipes"
 };
 
 const notLoggedIn: Media = {
   address: notLoggedInAddr,
-  altText: ""
+  altText: "Before logging into the site, users are presented with a login button"
 };
 
 const recipePage: Media = {
   address: recipePageAddr,
-  altText: ""
+  altText: "A user's recipe page"
+};
+
+const shoppingList: Media = {
+  address: shoppingListAddr,
+  altText: "A user's shopping list"
 };
 
 const storeMode: Media = {
   address: storeModeAddr,
-  altText: "",
+  altText: "Entering store mode on the shopping list",
   videoType: "video/ogg"
 };
 
 const images: {readonly [key: string]: Media} = {
-  category,
-  recipe,
-  ingredient,
-  authZero,
-  dragAndDrop,
-  homePage,
   notLoggedIn,
+  authZero,
+  homePage,
   recipePage,
+  recipe,
+  shoppingList,
+  ingredient,
+  category,
+  dragAndDrop,
   storeMode
 };
 
--- src/projectInfo/dinnerGetter.ts
+++ src/projectInfo/dinnerGetter.ts
@@ -15,7 +15,7 @@ const dinnerGetter: Project = {
   description: [],
   pitch: [
     "Not sure what you're doing for dinner tonight?  Already planned out every meal for the next two weeks?  Either way, Dinner Getter can help!",
-    "One feature of the app helps users add items to a customizable, categorized shopping list, and then tap items on the list to cross them off.  Another helps match users with recipes based on ingredients that they already have in their pantry."
+    "One feature of the app helps users add items to a customizable, categorized shopping list, and then tap items on the list to cross them off.  Another helps find recipes that users can make using ingredients that they already have in their pantry."
   ],
   spotlightMedia: {
     address: dinnerGetterSpotlight,
@@ -37,8 +37,54 @@ const dinnerGetter: Project = {
 };
 
 dinnerGetter.description.push({
-  text: "Reorganize your list with drag-and-drop functionality",
+  text: "Before logging in to the site, users will be presented with a button to log in.",
+  media: demoImages.notLoggedIn
+});
+
+dinnerGetter.description.push({
+  text: "The login button brings the user to a login page powered by Auth0.  Users can sign up with an email and password, or with an existing Google account.",
+  media: demoImages.authZero
+});
+
+dinnerGetter.description.push({
+  text: "The home page features the user's pantry (right), where they can enter ingredients that they have on hand.  Based on these ingredients, the app generates recipes that are a good match (left), indicating ingredients that are still missing and offering a button to add the missing ingredient to the user's shopping list.",
+  media: demoImages.homePage
+});
+
+dinnerGetter.description.push({
+  text: "The user can also add recipes to the site; any recipes that the user has added will appear on the Recipes page, shown here.",
+  media: demoImages.recipePage
+});
+
+dinnerGetter.description.push({
+  text: "When adding a recipe, users can add (and remove) as many ingredients as the recipe calls for, as well as the name of the recipe and the steps required to make it.",
+  media: demoImages.recipe
+});
+
+dinnerGetter.description.push({
+  text: "Each user also has a shopping list, which they can manage on this page.  Users can add categories, add new ingredients to any category, and remove any items from the list.",
+  media: demoImages.shoppingList
+});
+
+dinnerGetter.description.push({
+  text: "When in \"Edit Mode\", each category features an input field at the bottom, so users can easily add new ingredients to any category.  Each ingredient also has a delete button.",
+  media: demoImages.ingredient
+});
+
+dinnerGetter.description.push({
+  text: "Users can also add new categories to the list, rename existing categories, and delete categories.  Before deleting a category, they will be presented with a warning that all the ingredients in that category will also be deleted.",
+  media: demoImages.category
+});
+
+dinnerGetter.description.push({
+  text: "Drag and drop functionality makes it easy to reorder items on the list.  Items can be moved within their own category or dragged onto other categories.",
   media: demoImages.dragAndDrop
 });
+
+dinnerGetter.description.push({
+  text: "Dinner Getter also helps manage your grocery list while at the store: toggle the switch at the top to \"Store Mode\" to enable a feature that helps with the shopping.  When in Store Mode, simply click (or more likely touch, assuming most users will be on mobile while in the store) an item to cross it off.  Touch again to un-cross of an item.",
+  media: demoImages.storeMode
+});
+
 
 export default dinnerGetter;
